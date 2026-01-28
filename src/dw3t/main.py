5716b675a1627c1259b55a489353d0915bdec2ea
gwf <ysengrin9@gmail.com>
2026-01-28 14:56:21 +0100

    updated simulation.processing

--- src/dw3t/main.py
+++ src/dw3t/main.py
@@ -52,30 +52,60 @@ def main(argv: list[str] | None = None) -> int:
         config=config,
     )
 
-    if config["simulation"]["processing"] is not None:
-        processing_category = config["simulation"]["processing"].split(":")
-        if processing_category[0]=="template" and len(processing_category)>1:
+    if "processing" in config["simulation"]:
+        processing_dict = np.atleast_1d(config["simulation"]["processing"]).tolist()
+        processing_category = [d.get("mode") for d in processing_dict]
+        if "userdef" in processing_category:
+            if len(processing_category)!=1:
+                raise ValueError(
+                    "mode='userdef' cannot be combined with other processing modes."
+                )
+            processing_file = processing_dict[0].get("file")
+            if not processing_file.endswith(".py"):
+                raise ValueError(
+                    f"{proceprocessing_file=} should be a .py file. See example in dw3t/src/dw3t/template/userdef.py."
+                )
+            if not(os.path.isfile(processing_file)):
+                raise FileNotFoundError(f"absolute path of the file '{processing_file}' must exist.")
+            spec = importlib.util.spec_from_file_location(Path(processing_file).stem, processing_file)
+            template_modules = [importlib.util.module_from_spec(spec)]
+            spec.loader.exec_module(template_modules[0])
+        else: 
             template_modules = []
-            for ii in range(1, len(processing_category)):
+            for ii in range(len(processing_category)):
                 try:
                     template_modules.append(importlib.import_module(f"dw3t.template.{processing_category[ii]}", package=None))
                 except ModuleNotFoundError:
                     raise ModuleNotFoundError(
                         f"No module named 'template.{processing_category[ii]}'."
                     )
-        elif processing_category[0].endswith(".py") and len(processing_category)==1:
-            processing_file = processing_category[0]
-            if not(os.path.isfile(processing_file)):
-                raise FileNotFoundError(f"absolute path of the file '{processing_file}' must exist.")
-            spec = importlib.util.spec_from_file_location(Path(processing_file).stem, processing_file)
-            template_modules = [importlib.util.module_from_spec(spec)]
-            spec.loader.exec_module(template_modules[0])
-        else:
-            raise ValueError(
-                f"{processing_category=} should have the form 'template:func' or path_to_file/file.py."
-            )
         for tm in template_modules:
             model = tm.processing(model=model)
+
+    # if config["simulation"]["processing"] is not None:
+    #     processing_category = config["simulation"]["processing"].split(":")
+    #     if processing_category[0]=="template" and len(processing_category)>1:
+    #         template_modules = []
+    #         for ii in range(1, len(processing_category)):
+    #             try:
+    #                 template_modules.append(importlib.import_module(f"dw3t.template.{processing_category[ii]}", package=None))
+    #             except ModuleNotFoundError:
+    #                 raise ModuleNotFoundError(
+    #                     f"No module named 'template.{processing_category[ii]}'."
+    #                 )
+    #     elif processing_category[0].endswith(".py") and len(processing_category)==1:
+    #         processing_file = processing_category[0]
+    #         if not(os.path.isfile(processing_file)):
+    #             raise FileNotFoundError(f"absolute path of the file '{processing_file}' must exist.")
+    #         spec = importlib.util.spec_from_file_location(Path(processing_file).stem, processing_file)
+    #         template_modules = [importlib.util.module_from_spec(spec)]
+    #         spec.loader.exec_module(template_modules[0])
+    #     else:
+    #         raise ValueError(
+    #             f"{processing_category=} should have the form 'template:func' or path_to_file/file.py."
+    #         )
+    #     for tm in template_modules:
+    #         model = tm.processing(model=model)
 
     if model.dimension!=3:
         raise ValueError(f"{model.dimension=}D, should be 3D for RADMC3D. Try to post-process the data for it to be 3D.")
