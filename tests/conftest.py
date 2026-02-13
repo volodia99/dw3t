1c62247d85f36ea0ece3ffd6ab3fe1a8dd4f3dac
gwf <ysengrin9@gmail.com>
2026-02-13 16:00:58 +0100

    TST: added tests using datasets submodule

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,5 +3,9 @@ from pathlib import Path
 import pytest
 
 @pytest.fixture()
+def data_dir():
+    return Path(Path(__file__).parent).parent / "datasets"
+
+@pytest.fixture()
 def test_data_dir():
-    return Path(__file__).parent / "data"
+    return Path(__file__).parent
