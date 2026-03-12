BUG: fix bugs where ill-configured components could be silently ignored

src/dw3t/main.py
@@ -63,7 +63,7 @@ def main(argv: list[str] | None = None) -> int:
 
     component = config["simulation"]["component"]
     component = np.atleast_1d(component).tolist()
-    if not (set(component) & set(["gas","dust"])):
+    if not set(component).issubset({"gas","dust"}):
         raise ValueError(
             f"{component=} should be 'dust', 'gas' or ['dust', 'gas']."
         )