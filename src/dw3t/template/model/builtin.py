4df86eb66e7880711269c59d4bfcc3be51769759
volodia99 <ysengrin9@gmail.com>
2026-04-03 10:16:58 +0200

    CLN: update deprecated nonos functions

--- src/dw3t/template/model/builtin.py
+++ src/dw3t/template/model/builtin.py
@@ -24,8 +24,8 @@ def processing(*, model:"Model", **kwargs) -> "Model":
     input_dir = kwargs["input_dir"]
     ds = GasDataSet(file, directory=input_dir)
 
-    if ds.native_geometry!="spherical":
-        raise NotImplementedError(f"native_geometry={ds.native_geometry} should be 'spherical'.")
+    if ds.geometry!="spherical":
+        raise NotImplementedError(f"geometry={ds.geometry} should be 'spherical'.")
 
     UNIT_DENSITY = model.unit_mass_msun / model.unit_length_au**3
     UNIT_VELOCITY = np.sqrt(uc.G*model.unit_mass_msun/model.unit_length_au).to(u.m/u.s)
@@ -35,10 +35,10 @@ def processing(*, model:"Model", **kwargs) -> "Model":
     dust = model.dust
 
     grid = Grid(
-        x1=((ds.coords.get_axis_array("r") * model.unit_length_au).to(u.cm)),#.value,
-        x2=ds.coords.get_axis_array("theta") * u.radian,
-        x3=ds.coords.get_axis_array("phi") * u.radian,
-        geometry=ds.native_geometry,
+        x1=((ds.coordinates.get_axis_array("r") * model.unit_length_au).to(u.cm)),#.value,
+        x2=ds.coordinates.get_axis_array("theta") * u.radian,
+        x3=ds.coordinates.get_axis_array("phi") * u.radian,
+        geometry=ds.geometry,
     )
 
     if "gas" in model.component:
